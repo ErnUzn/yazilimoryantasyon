4acb84cebec51f372423e0520234f6c0924339d8
ErnUzn <erenuzun233@gmail.com>
2022-12-11 20:15:17 +0300

    yazılım oryantasyon sınav
    https://www.youtube.com/channel/UCveLgz26DrCRZ0shCxv7MHw

---
+++ yazilimoryantasyon/JavaApplication33/src/javaapplication33/JavaApplication33.java
@@ -0,0 +1,116 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication33;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ramazan
+ */
+public class JavaApplication33 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // ahmet eren uzun 220541108
+       // n'e kadar olan çift sayıların toplamı
+      /* Scanner s = new Scanner(System.in);
+       System.out.println("1 sayı giriniz");
+       int sayi = s.nextInt();
+       int c=0;
+        for (int i = 0; i <= sayi; i++) {
+            if (i%2==0) {
+                c+=i;
+            }
+        }
+    System.out.println("n'e kadar olan çift sayıların toplamı = "+c);*/
+ 
+    
+     //varış süresi hesaplama 
+     /* Scanner s = new Scanner(System.in);
+      System.out.println("aracın varis noktasina olan uzakligi");
+      int sayi1 = s.nextInt();
+      System.out.println("aracin hizi");
+      int sayi2 = s.nextInt();
+      double c = sayi1/sayi2;
+      System.out.println("aracin varis suresi = "+c);*/
+      //aracın varış süresi ondalıklı olur diye double ile tanımladık
+    
+    // dikdörtgenin çevresi
+/*Scanner s = new Scanner(System.in);
+System.out.println("uzun kenar");
+int sayi = s.nextInt();
+    System.out.println("kısa kenar");
+    int sayi2 = s.nextInt();
+    int c =(sayi+sayi2)*2;
+    System.out.print(c);
+  */  
+     // dairenin alanı
+       /* Scanner s = new Scanner(System.in);
+        System.out.println("dairenin yaricapi");
+        int sayi = s.nextInt();
+        double pi=3.14;
+        double c =sayi*pi;
+        System.out.println("alan = "+c);
+    
+    */
+    
+    // darinenin çevresi
+      /*  Scanner s =new Scanner(System.in);
+       System.out.println("darinenin yaricapi");
+    int sayi = s.nextInt();
+    double pi = 3.14;
+    double c = sayi*pi*2;
+    System.out.println("dairenin cevresi = "+c);*/
+    
+     //n'e kadar olan sayilarin toplami
+      /*  Scanner s = new Scanner(System.in);
+        System.out.println("bir sayi giriniz");
+    int sayi=s.nextInt();
+    int c = 0;
+        for (int i = 0; i < sayi; i++) {
+            c+=i;
+        }
+            System.out.println("n'e kadar olan sayıların toplami = "+c);
+            //for döngüsüyle girilen sayıya kadar olan sayıların toplamını c değişkenine tanımlayıp topladık
+            
+    */
+     //girilen 2 sayını toplamı 
+      /*  Scanner s = new Scanner(System.in);
+     System.out.println("1.sayıyı giriniz");
+     int sayi1 = s.nextInt();
+     System.out.println("2.sayıyı giriniz");
+     int sayi2 = s.nextInt();
+     int c = sayi1+sayi2;
+     System.out.println("2 sayının toplamı = "+c);*/
+    
+    
+    // 2 sayının toplamı
+/*int a = 1;
+int b = 2;
+int c = a+b;
+System.out.println("2 sayının toplamı = "+c);
+*/
+     //2 sayının çarpımı 
+      /*  int a = 5;
+        int b = 10;
+        int c = a*b;
+        System.out.println("2 sayinin çarpimi = "+c);
+    */
+    // girilen 2 sayının çarpımı
+/*Scanner s =new Scanner(System.in);
+System.out.println("1.sayı");
+int sayi = s.nextInt();
+System.out.println("2.sayı");
+int sayi2= s.nextInt();
+int c = sayi*sayi2;
+System.out.println("2 sayınını çarpımı = "+c);
+    */
+    
+    }
+    
+}
